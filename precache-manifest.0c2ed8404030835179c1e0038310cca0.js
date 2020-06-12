ac9bf276192b3f05525a8be0d8eca9ab1ee8cba9
Yasser Ansari <yasser.ansari@jhc.co.uk>
2020-06-12 14:34:25 +0100

    Updates

---
+++ precache-manifest.0c2ed8404030835179c1e0038310cca0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1414f05ac9bf46c75a1dd778257e4d3a",
+    "url": "/recipes/index.html"
+  },
+  {
+    "revision": "8905ad63a9010ee874aa",
+    "url": "/recipes/static/js/2.c92ba3e1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/recipes/static/js/2.c92ba3e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "444ed0cca26b2150421a",
+    "url": "/recipes/static/js/main.31d9e35f.chunk.js"
+  },
+  {
+    "revision": "e2dd81471422ab2378d8",
+    "url": "/recipes/static/js/runtime-main.b4e7e742.js"
+  }
+]);
---
+++ static/js/main.31d9e35f.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonprecipes=this.webpackJsonprecipes||[]).push([[0],{7:function(e,t,n){e.exports=n(8)},8:function(e,t,n){"use strict";n.r(t);var r=n(2),c=n(3),a=n(5),u=n(6),o=n(0),i=n.n(o),s=n(4),p=n.n(s),l=function(e){Object(u.a)(n,e);var t=Object(a.a)(n);function n(){return Object(r.a)(this,n),t.apply(this,arguments)}return Object(c.a)(n,[{key:"render",value:function(){return i.a.createElement("div",null,"Test")}}]),n}(i.a.Component);p.a.render(i.a.createElement(l,null),document.querySelector("#root"))}},[[7,1,2]]]);
+//# sourceMappingURL=main.31d9e35f.chunk.js.map
