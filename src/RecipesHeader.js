8b7bc8d735bc83fe4ba9ec4938454ba79b0647fb
Yasser Ansari <yasser.ansari@jhc.co.uk>
2020-06-12 15:36:36 +0100

    adding header and styling header

--- src/RecipesHeader.js
+++ src/RecipesHeader.js
@@ -1,7 +1,16 @@
+import "./RecipesHeader.css";
 import React from "react";
 
 const RecipesHeader = () => {
-  return <div>Welcome to Mama's Recipes</div>;
+  return (
+    <div className="recipes-header">
+      <h1 className="ui header">
+        <i className="massive icon utensils" />
+        <div className="content">Welcome to Mama's Recipes!</div>
+        <i className="massive icon coffee" />
+      </h1>
+    </div>
+  );
 };
 
 export default RecipesHeader;
